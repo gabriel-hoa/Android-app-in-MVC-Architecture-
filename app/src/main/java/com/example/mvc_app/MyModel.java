7e10e23eddef2b4a2258bc3a8b74cb6b7fd5130d
gabriel-hoa <gabrielhoaraujo100@gmail.com>
2023-11-29 14:43:48 -0300

    model

---
+++ app/src/main/java/com/example/mvc_app/MyModel.java
@@ -0,0 +1,41 @@
+package com.example.mvc_app;
+
+//MVC- Model/View/Controler
+// Model - Is the application backend. Acts like the Database or Network(Cloud store)
+public class MyModel {
+
+    String appName;
+    int appDownloads;
+    int appRating;
+
+    public MyModel(String appName, int appDownloads, int appRating) {
+        this.appName = appName;
+        this.appDownloads = appDownloads;
+        this.appRating = appRating;
+    }
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void getAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public int getAppDownloads() {
+        return appDownloads;
+    }
+
+    public void setAppDownloads(int appDownloads){
+        this.appDownloads = appDownloads;
+    }
+
+    public int getAppRating(){
+        return appRating;
+    }
+
+    public void setAppRating(int appRating) {
+        this.appRating = appRating;
+    }
+
+}
